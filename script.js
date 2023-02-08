7f5e7b72b3eabcfc7769615047bdf2f188222814
vandaminator <105977946+vandaminator@users.noreply.github.com>
2023-02-08 22:46:45 +0200

    lets go

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// add value
 function toScreen(number) {
     window.alert('number pressed was ' + number)
 }
+
+
+
+function clearScreen() {
+    window.alert('screen cleared')
+}
+const clearBtnElement = document.querySelector('.clear-button');
+clearBtnElement.addEventListener('click', clearScreen);
+
+
+
+// this checks if the brackets are closed or note
+let groupOut = true;
+function parenthisisFunc () {
+    if (groupOut) {
+        window.alert('we are in');
+        groupOut = false;
+    } else {
+        window.alert('we are out');
+        groupOut = true;
+    }
+}
+const parenthisElement = document.querySelector('.parnthesis');
+parenthisElement.addEventListener('click', parenthisisFunc);
+
+
+function percentageFunc() {
+    window.alert('made percentage');
+}
+const percentageElement = document.querySelector('.percentage');
+percentageElement.addEventListener('click', percentageFunc);
